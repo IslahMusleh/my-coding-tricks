1fd960f4f4b6ce594e8bda55be45138f23524851
Islah Mohammad <islah222musleh@gmail.com>
2018-11-30 21:38:37 +0800

    disabling a bootstrap modal to close in certain cases

--- JavaScript-tricks.js
+++ JavaScript-tricks.js
@@ -1,1 +1,14 @@
-This file includes JavaScript and Jquery tricks I faced
+// This file includes JavaScript and Jquery tricks I faced
+<script>
+{/* disabling a bootstrap modal to close in certain cases. */}
+// bootstrap 3
+$('#pushToGitModal').data('bs.modal').options.backdrop = 'static';
+$('#pushToGitModal').data('bs.modal').options.keyboar = false;
+
+// bootstrap 4
+$('#pushToGitModal').data('bs.modal')._config.backdrop = 'static';
+$('#pushToGitModal').data('bs.modal')._config.keyboar = false;
+
+
+
+</script>
